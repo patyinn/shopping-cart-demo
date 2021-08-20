d6ca54cb0f00138765361ec5e709e1d567b7423e
patyinn <85993427+patyinn@users.noreply.github.com>
2021-08-20 17:22:03 +0800

    Update settings.py

--- plant_website/plant_website/settings.py
+++ plant_website/plant_website/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'plantdb',
         'USER': 'django-admin',
-        'PASSWORD': 'bpxu31p4204',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
