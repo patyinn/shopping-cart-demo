cd8267cd45939ca40abeba4de517dbdd49a097f1
patyinn <master080000@gmail.com>
2023-01-24 11:17:34 +0800

    1. change folder name

--- website_demo/main_setting/asgi.py
+++ website_demo/main_setting/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for main_website project.
+ASGI config for main_setting project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main_website.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main_setting.settings')
 
 application = get_asgi_application()
--- website_demo/main_setting/wsgi.py
+++ website_demo/main_setting/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for main_website project.
+WSGI config for main_setting project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main_website.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main_setting.settings')
 
 application = get_wsgi_application()
